c9a21b02fb026f788b9a4f01fab5a12d9bb3d776
Stuart Mathews <stumathews@gmail.com>
2015-02-12 15:54:33 +0000

    more useful scripts

---
+++ Receiver/swapuser.ps1
@@ -0,0 +1,60 @@
+﻿function swapinuser( $username )
+{
+    Write-Host "* Switching in user, $username..."
+    & "C:\Program Files (x86)\Citrix\CtxCredApiSample.exe" -l -u $username -p citrix -d xa.local
+    dropAuth 
+    ipoll
+}
+
+function dropAuth()
+{
+    Write-host "* Dropping SSP credentials via Authman reset"
+    & "C:\Program Files (x86)\Citrix\SelfServicePlugin\SelfService.exe" -terminate
+}
+
+function ipoll()
+{
+    Write-Host "* Performing interactive poll"
+    & "C:\Program Files (x86)\Citrix\SelfServicePlugin\SelfService.exe" -ipoll
+}
+
+function logoff()
+{
+    Write-Host "Logging off user"
+    & "C:\Program Files (x86)\Citrix\CtxCredApiSample.exe" -s
+    Write-Host "There should be no pending http activity now"
+}
+
+$array = 1..10
+
+foreach( $i in $array )
+{
+
+    swapinuser -username "user1"
+    logoff
+
+
+    swapinuser -username "user2"
+    logoff
+    
+    <#
+
+    if ( [bool]!($i%2) )
+    {
+        Write-Host "###Inserting bad creds##"
+       
+        swapinuser -username "bad"         
+        logoff
+        Write-Host "Waiting 5 secs for prompt before dismissing via -terminate"
+        Start-Sleep -s 5
+        Start-Job -ScriptBlock { Write-host "** Dropping SSP credentials via Authman reset"
+                                 & "C:\Program Files (x86)\Citrix\SelfServicePlugin\SelfService.exe" -terminate | out-null  
+                                } | Wait-Job
+        Write-Host "job done"
+        Get-Command
+
+    
+    }
+    #>    
+}
+
---
+++ remote_ps.ps1
@@ -0,0 +1,67 @@
+﻿. .\useful_functions.ps1
+
+function Get-IsTrusted($RemoteComputerName)
+{
+    $trusted_hosts = (Get-Item WSMan:\localhost\Client\TrustedHosts).Value
+
+    if( $trusted_hosts.Contains($RemoteComputerName) -eq $true )
+    {       
+
+        return [bool]$true;
+    }
+    else
+    {
+        return [bool]$false;
+    }
+}
+
+#Trap [SystemException] { "Could not create remote session: $error, $StackTrace" ; continue }
+function CreateRemoteSession($RemoteComputerName)
+{
+
+    if( (Get-IsTrusted($RemoteComputerName)) -eq $false)
+    {
+        Write-Host "Won't connect to host that is not trusted. Add it to trusted hosts."
+        return
+    }
+    
+    $remote_session = New-PSSession -ComputerName $RemoteComputerName -authentication Default -credential (Get-Credential)
+    return $remote_session
+        
+}
+
+function EnterRemoteSession($session)
+{
+    Enter-PSSession -Session $session
+}
+
+
+function EndRemoteSession($Session)
+{
+    Remove-PSSession -Session $Session
+}
+
+function InvokeCommandOnRemote()
+{
+    [cmdletbinding(SupportsShouldProcess=$True)]
+    param(
+        [parameter(Mandatory=$true)]
+        $Session,
+
+        [parameter(Mandatory=$true)] 
+        [string] $scriptBlock 
+        )
+
+    if( $args.Count -gt 0 ) { Write-Error "unhandled arguments supplied"} #$args is eveyrthing else after the 2 arguments I expect.
+
+
+    $block = [ScriptBlock]::Create($scriptBlock)
+
+    Write-Verbose "Invoking command, $block on remote host $Session.ComputerName"
+
+    $result = Invoke-Command -Session $Session -ScriptBlock $block
+    
+    return $result
+}
+
+
